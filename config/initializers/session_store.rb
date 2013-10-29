740bfd36b29a0fead5b8cbac613be5212bd6fab1
Michael Frister <m.frister@finn.de>
2013-10-29 15:56:15 +0100

    Use active_record_store for sessions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -28,7 +28,7 @@
 
 # Be sure to restart your server when you modify this file.
 
-OpenProject::Application.config.session_store :cookie_store, :key => '_open_project_session'
+OpenProject::Application.config.session_store :active_record_store
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
